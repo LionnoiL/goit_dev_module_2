f279befe4cbb4a4a6b6b9de9ee04f7665f27fdef
gaponov <wmconstructor@gmail.com>
2023-01-06 20:23:29 +0200

    Add main method in SumCalculator

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,5 +1,9 @@
 public class SumCalculator {
 
+    public static void main(String[] args) {
+        System.out.println(new SumCalculator().sum(5));
+    }
+
     public int sum(int number) {
         if (number<=0){
             throw new IllegalArgumentException("Число повинно бути більше нуля");
