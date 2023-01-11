18d517fe42e1f558607e41e66812422f11189ed2
gaponov <wmconstructor@gmail.com>
2023-01-11 18:59:33 +0200

    Delete method main from SumCalculator

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,9 +1,5 @@
 public class SumCalculator {
 
-    public static void main(String[] args) {
-        System.out.println(new SumCalculator().sum(5));
-    }
-
     public int sum(int number) {
         if (number<=0){
             throw new IllegalArgumentException("Число повинно бути більше нуля");
